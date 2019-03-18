14a26b6ccaa37ca221857f9b740993447e0d1189
Frank Mitchell <48015596+fmitchell259@users.noreply.github.com>
2019-03-18 21:14:15 +0000

    Update networkMaster.py

--- networkMaster.py
+++ networkMaster.py
@@ -672,7 +672,7 @@ def main():
     print('===========================')
     print('Below is a list of 36 entries. Each entry represents a journey in the network, in order (1-1,1-2,1-3 etc).')
     print('Within each list index is a tuple containing two more lists, the second representing how quickly a packet arrived and the left representing the node that packet must go through.')
-    print('Some added functionality to remove duplicate nectBestNodes needs to be added')
+    print('Some added functionality to remove duplicate nextBestNodes needs to be added')
     print('')
     print('Best journey times list: ' + str(finalBestList))
 
